dcc8d352678f28afd51fc95dad0251e168f08b81
Josiah Allen <josiah_g_allen@yahoo.com>
2015-09-21 22:24:54 -0500

    finished hard mode, there is a glitch in HTML when stats added, extra white space between th and tbody apear

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -12,29 +12,33 @@ function Quarterback(name, team) {
 	this.sacks = 0;
 	this.longestCompletion = 0;
 	this.completionPercentage = function () {
-		return 'Completion percentage: ' + Math.round(this.completions / this.attempts * 100);
+		if (this.attempts !== 0) {
+			return Math.round(this.completions / this.attempts * 100);
+		} else {
+			return 0;
+		}
 	};
 	this.yardsPerAttempt = function () {
 		return 'Yards per attempt: ' + (this.yards / this.attempts).toFixed(2);
 	};
 	this.addAttempt = function (type, yards) {
-		if (type === 'completion') {
+		if (type === 'completions') {
 			this.attempts++;
 			this.completions++;
 			this.yards += yards;
-		} else if (type === 'touchdown') {
+		} else if (type === 'touchdowns') {
 			this.attempts++;
 			this.completions++;
 			this.yards += yards;
 			this.touchdowns++;
-		} else if (type === 'interception') {
+		} else if (type === 'interceptions') {
 			this.attempts++;
-			this.interceptions = 0;
-		} else if (type === 'sack') {
+			this.interceptions++;
+		} else if (type === 'sacks') {
 			this.yards += yards;
 			this.sacks++;
 		} else {
-			this.attemps++;
+			this.attempts++;
 		}
 
 		if (yards > this.longestCompletion) {
@@ -42,12 +46,11 @@ function Quarterback(name, team) {
 		}
 	};
 	this.displayStats = function () {
-		$('#QB').append('<tr><td>' + this.name + '</td><td>' + this.team + '</td><td>' + this.completions + '</td><td>' + this.attempts + '</td><td>' + this.yards + '</td><td>' + this.touchdowns + '</td><td>' + this.interceptions + '</td><td>' + this.sacks + '</td><td>' + this.longestCompletion + '</td></tr>');
+		$('#QB').append('<tr class="newQdata"><td>' + this.name + '</td><td>' + this.team + '</td><td>' + this.completions + '</td><td>' + this.attempts + '</td><td>' + this.completionPercentage() + '</td><td>' + this.yards + '</td><td>' + this.touchdowns + '</td><td>' + this.interceptions + '</td><td>' + this.sacks + '</td><td>' + this.longestCompletion + '</td></tr>');
 	};
-	this.upDateStats = function () {};
 }
 
-var peytonManning = new Quarterback('Peyotn Manning', 'Denver Broncos');
+var peytonManning = new Quarterback('Peyton Manning', 'Denver Broncos');
 var tonyRomo = new Quarterback('Tony Romo', 'Dallas Cowboys');
 var qbs = [peytonManning, tonyRomo];
 
@@ -66,15 +69,19 @@ function Runningback(name, team) {
 	this.fumbles = 0;
 	this.longestRush = 0;
 	this.yardsPerAttempt = function () {
-		return (this.yards / this.attempts).toFixed(2);
+		if (this.attempts !== 0) {
+			return (this.yards / this.attempts).toFixed(2);
+		} else {
+			return 0;
+		}
 	};
 	this.addAttempt = function (type, yards) {
 		if (type === 'rush') {
 			this.yards += yards;
-		} else if (type === 'touchdown') {
+		} else if (type === 'touchdowns') {
 			this.touchdowns++;
 			this.yards += yards;
-		} else if (type === 'fumble') {
+		} else if (type === 'fumbles') {
 			this.fumbles++;
 		} else if (type === 'firstDowns') {
 			this.yards += yards;
@@ -92,11 +99,11 @@ function Runningback(name, team) {
 		}
 	};
 	this.displayStats = function () {
-		$('#RB').append('<tr><td>' + this.name + '</td><td>' + this.team + '</td><td>' + this.attempts + '</td><td>' + this.yards + '</td><td>' + this.touchdowns + '</td><td>' + this.firstDowns + '</td><td>' + this.over20 + '</td><td>' + this.longestRush + '</td><td>' + this.fumbles + '</td></tr>');
+		$('#RB').append('<tr class="newRdata"><td>' + this.name + '</td><td>' + this.team + '</td><td>' + this.attempts + '</td><td>' + this.yards + '</td><td>' + this.yardsPerAttempt() + '</td><td>' + this.touchdowns + '</td><td>' + this.firstDowns + '</td><td>' + this.over20 + '</td><td>' + this.longestRush + '</td><td>' + this.fumbles + '</td></tr>');
 	};
 }
 
-var marshawnLynch = new Runningback('Marshawn Lynch', 'Seattle');
+var marshawnLynch = new Runningback('Marshawn Lynch', 'Seattle Seahawks');
 var adrianPetterson = new Runningback('Adrian Peterson', 'Minnesota Vikings');
 var rbs = [marshawnLynch, adrianPetterson];
 
@@ -111,21 +118,25 @@ function Kicker(name, team) {
 	this.made = 0;
 	this.longestFieldGoal = 0;
 	this.fieldGoalPercentage = function () {
-		return Math.round(this.attempts / this.made);
+		if (this.attempts !== 0) {
+			return Math.round(this.made / this.attempts * 100);
+		} else {
+			return 0;
+		}
 	};
 	this.addAttempt = function (type, yards) {
 		if (type === 'made') {
 			this.made++;
 		}
 
-		this.attempt++;
+		this.attempts++;
 
 		if (yards > this.longestFieldGoal) {
 			this.longestFieldGoal = yards;
 		}
 	};
 	this.displayStats = function () {
-		$('#K').append('<tr><td>' + this.name + '</td><td>' + this.team + '</td><td>' + this.made + '</td><td>' + this.attempts + '</td><td>' + this.longestFieldGoal + '</td></tr>');
+		$('#K').append('<tr class="newKdata"><td>' + this.name + '</td><td>' + this.team + '</td><td>' + this.made + '</td><td>' + this.attempts + '</td><td>' + this.fieldGoalPercentage() + '</td><td>' + this.longestFieldGoal + '</td></tr>');
 	};
 }
 
@@ -136,6 +147,53 @@ var ks = [danBailey, masonCrosby];
 for (var i = 0; i < ks.length; i++) {
 	ks[i].displayStats();
 }
+
+$('#qbForm').submit(function (e) {
+	e.preventDefault();
+	var yards = parseFloat($('#passYards').val());
+	if ($('#qbDropDown option:selected').text() === 'Manning') {
+		peytonManning.addAttempt($('#passType').val(), yards);
+	}
+	if ($('#qbDropDown option:selected').text() === 'Romo') {
+		tonyRomo.addAttempt($('#passType').val(), yards);
+	}
+	$('.newQdata').html('');
+	for (var i = 0; i < qbs.length; i++) {
+		qbs[i].displayStats();
+	}
+});
+
+$('#rbForm').submit(function (e) {
+	e.preventDefault();
+	var yards = parseFloat($('#rushYards').val());
+	console.log(yards);
+	if ($('#rbDropDown option:selected').text() === 'Lynch') {
+		marshawnLynch.addAttempt($('#rushType').val(), yards);
+	}
+	if ($('#rbDropDown option:selected').text() === 'Peterson') {
+		adrianPetterson.addAttempt($('#rushType').val(), yards);
+	}
+	$('.newRdata').html('');
+	for (var i = 0; i < rbs.length; i++) {
+		rbs[i].displayStats();
+	}
+});
+
+$('#kForm').submit(function (e) {
+	e.preventDefault();
+	var yards = parseFloat($('#kYards').val());
+	console.log(yards);
+	if ($('#kDropDown option:selected').text() === 'Bailey') {
+		danBailey.addAttempt($('#kickType').val(), yards);
+	}
+	if ($('#kDropDown option:selected').text() === 'Crosby') {
+		masonCrosby.addAttempt($('#kickType').val(), yards);
+	}
+	$('.newKdata').html('');
+	for (var i = 0; i < ks.length; i++) {
+		ks[i].displayStats();
+	}
+});
 
 },{}]},{},[1])
 
